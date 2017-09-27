7d855b89e455ab4b9b419058b7e6cce7d6023031
vishnusasuke <vishnusasuke@gmail.com>
2017-09-27 13:56:06 -0500

    initial

---
+++ WebApplication7/WebApplication7/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace WebApplication7.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /Student/
+        public ActionResult Student()
+        {
+            Student s = new Student();
+            s.firstname = "Vishnu";
+            s.lastname = "Vardhan";
+            return View(s);
+        }
+	}
+}
---
+++ WebApplication7/WebApplication7/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApplication7
+{
+    public class Student
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+
+    }
+}
